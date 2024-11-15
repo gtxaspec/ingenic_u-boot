777dda933f5eee8b260785bc4cd38dbdec0440b8
gtxaspec <gtxent@gmail.com>
2024-11-14 20:07:30 -0800

    cmd_net.c: backport from t31

--- common/cmd_net.c
+++ common/cmd_net.c
@@ -2,23 +2,7 @@
  * (C) Copyright 2000
  * Wolfgang Denk, DENX Software Engineering, wd@denx.de.
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307 USA
+ * SPDX-License-Identifier:	GPL-2.0+
  */
 
 /*
@@ -53,6 +37,12 @@ int do_tftpb(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 U_BOOT_CMD(
 	tftpboot,	3,	1,	do_tftpb,
+	"boot image via network using TFTP protocol",
+	"[loadAddress] [[hostIPaddr:]bootfilename]"
+);
+
+U_BOOT_CMD(
+	tftp,	3,	1,	do_tftpb,
 	"boot image via network using TFTP protocol",
 	"[loadAddress] [[hostIPaddr:]bootfilename]"
 );
@@ -284,11 +274,11 @@ static int do_ping(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		return CMD_RET_USAGE;
 
 	if (NetLoop(PING) < 0) {
-		printf("ping failed; host %s is not alive\n", argv[1]);
+		printf("Ping failed; host %s is not alive\n", argv[1]);
 		return 1;
 	}
 
-	printf("host %s is alive\n", argv[1]);
+	printf("Host %s is alive\n", argv[1]);
 
 	return 0;
 }
